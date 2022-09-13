59129e9fb1dd62a5faede2b5bd231b117f07fc9f
kngmovies <74289178+kngmovies@users.noreply.github.com>
2022-09-13 19:06:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -241,7 +241,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/UC6dB3PPjg1KmIW_1r7YUuCA?') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
